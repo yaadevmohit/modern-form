5f6f44562a5a4fd1d00cf06c04c10a748621fe6e
Mohit yadav <mohityadav530@gmail.com>
2023-07-14 12:39:16 -0400

    stage 1 set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='form-container'>
+        <div className='form-stage-container'>
+          <button className='form-stage'>1</button>
+          <button className='form-stage'>2</button>
+          <button className='form-stage'>3</button>
+          <button className='form-stage'>4</button>
+        </div>
+        <form className='main-form'>
+          <div className='stage-heading'>
+            <h2>Personal info</h2>
+            <span>Please provide your name, email address and phone number.</span>
+          </div>
+          <div className='form-field'>
+            <legend for='name'>Name</legend>
+            <input type='text' id='name' name='name' placeholder='e.g. Stehpen king'/>
+          </div>
+          <div className='form-field'>
+            <legend for='email'>Email Address</legend>
+            <input type='email' id='email' name='email' placeholder='e.g. stephen@lorem.ipsum'/>
+          </div>
+          <div className='form-field'>
+            <legend for='phone'>Phone Number</legend>
+            <input type='tel' id='phone' name='phone' placeholder='e.g. 111111111'/>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
