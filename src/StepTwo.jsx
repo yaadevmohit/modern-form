9bf5b7fac75a6095aafc6b43c5ee637a65b04f66
Mohit yadav <mohityadav530@gmail.com>
2023-07-21 12:00:41 -0400

    add step 3 yearly toggle

--- src/StepTwo.jsx
+++ src/StepTwo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './assets/fonts/Ubuntu-Bold.ttf'
 import arcadeSvg from './assets/images/icon-arcade.svg'
 import advancedSvg from './assets/images/icon-advanced.svg'
@@ -6,14 +6,62 @@ import proSvg from './assets/images/icon-pro.svg'
 import './App.css'
 import Option from './Option'
 
+const options = {
+    Arcade: {
+        name: "Arcade",
+        logo: arcadeSvg,
+        moPrice: "$9/mo",
+        yrPrice: "$100/yr",
+    },
+    Advanced: {
+        name: "Advanced",
+        logo: advancedSvg,
+        moPrice: "$12/mo",
+        yrPrice: "$120/yr",
+    },
+    Pro: {
+        name: "Pro",
+        logo: proSvg,
+        moPrice: "$12/mo",
+        yrPrice: "$120/",
+    }
+
+}
 
 
-const StepTwo = () => {
+
+const StepTwo = ( {options} ) => {
+
+    const [isYearly, setIsYearly] = useState(false);
+
+    function handleToggle() {
+        setIsYearly(!isYearly)
+    }
+
     return(
-        <>
-            <Option inputName="Arcade" logo={arcadeSvg} nameDetail="$9/mo" type="radio"/>
-            <Option inputName="Advanced" logo={advancedSvg} nameDetail="$12/mo" type="radio"/>
-            <Option inputName="Pro" logo={proSvg} nameDetail="$15/mo" type="radio"/>
+        <>  
+            {Object.values(options.map((option, index) =>(
+                    <>
+                    <Option 
+                            key={index}
+                            inputName={option.name}
+                            logo={option.log}
+                            nameDetail={isYearly ? option.yrPrice : option.moPrice}
+                            type="radio"
+                        />
+                    </>
+                    
+                ))
+            
+            )}
+            <div className='interval-toggle'>
+                <span>Monthly</span>
+                <label className='switch'>
+                    <input type="checkbox" onChange={handleToggle}/>
+                    <span className='slider'></span>
+                </label>
+                <span>Yearly</span>
+            </div>
         </>
     )
 
