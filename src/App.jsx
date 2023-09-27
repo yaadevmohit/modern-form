0227d466fa63a3ab54b9174bebaddc25b9cc1823
Mohit yadav <mohityadav530@gmail.com>
2023-09-26 22:50:20 -0400

    error description update

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () =>  {
         if(!formData.phone.trim()) {
           validationErrors.phone= "Phone is required"
         } else if(!/^\d+$/.test(formData.phone) || formData.phone.length !== 10) {
-          validationErrors.phone= "Phone number is not valid"
+          validationErrors.phone= "Phone number is not valid(should be 10 digits long)"
         }
         setErrors(validationErrors)
         if(Object.keys(validationErrors).length === 0) {
