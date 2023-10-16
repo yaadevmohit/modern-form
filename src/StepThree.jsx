8420f64299e70304e3c5a9c478e422a2eada0d62
Kyle Christensen <kylehgc@gmail.com>
2023-10-15 22:20:26 -0400

    committing changes for revert

--- src/StepThree.jsx
+++ src/StepThree.jsx
@@ -1,35 +1,46 @@
-import React, { useState } from 'react'
-import data from "./assets/formData"
+import data from './assets/formData'
 import StepInfo from './StepInfo'
 
 const optionsData = data.stepThreeOptions
 
-const StepThree = ({yearly, handleChange, selectedAddOns}) => {
-
-    const options = optionsData.map(option => {
-        return(
-            <div 
-                className={`option-container-three ${selectedAddOns.includes(option.id) && "option-container-selected"}`} 
-                key={option.id}
-            >
-                <input type="checkbox" name={option.id} value={option.id} onChange={handleChange} checked={selectedAddOns.includes(option.id)}/>
-                <label className='option' htmlFor={option.id}>
-                    {option.name}
-                    <span>{option.info}</span>
-                </label>
-                <span className='add-price'>{yearly ? `+$${option.yrPrice}/yr` : `+$${option.moPrice}/mo`}</span>
-            </div>
-        )
-    })
-    return(
-        <>
-            <StepInfo 
-                heading="Pick add-ons" 
-                about="Add-ons help enhance your gaming experience."
-            />
-            {options}
-        </>
-    )
+// eslint-disable-next-line react/prop-types
+const StepThree = ({ yearly, handleChange, selectedAddOns }) => {
+	const options = optionsData.map((option) => {
+		return (
+			<div
+				className={`option-container-three ${
+					// eslint-disable-next-line react/prop-types
+					selectedAddOns.includes(option.id) && 'option-container-selected'
+				}`}
+				key={option.id}
+			>
+				<input
+					type="checkbox"
+					name={option.id}
+					value={option.id}
+					onChange={handleChange}
+					// eslint-disable-next-line react/prop-types
+					checked={selectedAddOns.includes(option.id)}
+				/>
+				<label className="option" htmlFor={option.id}>
+					{option.name}
+					<span>{option.info}</span>
+				</label>
+				<span className="add-price">
+					{yearly ? `+$${option.yrPrice}/yr` : `+$${option.moPrice}/mo`}
+				</span>
+			</div>
+		)
+	})
+	return (
+		<>
+			<StepInfo
+				heading="Pick add-ons"
+				about="Add-ons help enhance your gaming experience."
+			/>
+			{options}
+		</>
+	)
 }
 
-export default  StepThree
+export default StepThree
